7fac00bda2820efaea7ce69efa4b4a0eea4eb192
Thibaut <thibaut2saivre@gmail.com>
2025-04-12 01:10:50 +0200

    Parse file as cli argument

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Plot Monitor
 
 A simple TUI to watch and plot `jsonl` training logs in real time.
+
+Examle usage:
+
+```bash
+cargo run -- examples/logs.jsonl
+```
--- pm-cli/src/main.rs
+++ pm-cli/src/main.rs
@@ -1,7 +1,18 @@
+use clap::Parser;
 use pm_lib::Logs;
 
+/// Simple TUI to monitor logs from JSONL files
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the JSONL file
+    path: String,
+}
+
 fn main() {
-    let log_path = "examples/logs.jsonl";
-    let logs = Logs::from_file(log_path);
-    println!("Logs: {:#?}", logs);
+    let args = Args::parse();
+
+    let logs = Logs::from_file(args.path.as_str());
+
+    println!("{:?}", logs);
 }
