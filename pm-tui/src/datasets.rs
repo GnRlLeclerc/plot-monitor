5448c555e706a146d73a4b3b20fa195ad9810ec1
Thibaut <thibaut2saivre@gmail.com>
2025-04-12 22:01:08 +0200

    Display proper labels

--- pm-tui/src/datasets.rs
+++ pm-tui/src/datasets.rs
@@ -23,10 +23,10 @@ pub fn to_dataset<'a>(name: &'a str, points: &'a [(f64, f64)]) -> Dataset<'a> {
 
 /// Draw datasets on the given area
 pub fn draw_datasets(logs: &Logs, rect: Rect, buf: &mut Buffer) {
-    let mut x_min = 0.0_f64;
-    let mut x_max = 0.0_f64;
-    let mut y_min = 0.0_f64;
-    let mut y_max = 0.0_f64;
+    let mut x_min = f64::INFINITY;
+    let mut x_max = f64::NEG_INFINITY;
+    let mut y_min = f64::INFINITY;
+    let mut y_max = f64::NEG_INFINITY;
 
     let mut cmap = Colormap::new();
 
@@ -44,22 +44,28 @@ pub fn draw_datasets(logs: &Logs, rect: Rect, buf: &mut Buffer) {
         })
         .collect::<Vec<_>>();
 
-    // TODO: compute labels and space them properly
-
     Chart::new(datasets)
         .x_axis(
             Axis::default()
                 .title("Epochs")
                 .style(Style::default().gray())
                 .bounds([x_min, x_max])
-                .labels([format!("{}", x_min as i64), format!("{}", x_max as i64)]),
+                .labels([
+                    format!("{}", x_min as i64),
+                    format!("{}", ((x_max + x_min) / 2.0) as i64),
+                    format!("{}", x_max as i64),
+                ]),
         )
         .y_axis(
             Axis::default()
                 .title("Value")
                 .style(Style::default().gray())
                 .bounds([y_min, y_max])
-                .labels([format!("{:5.3}", y_min), format!("{:5.3}", y_max)]),
+                .labels([
+                    format!("{:.2e}", y_min),
+                    format!("{:.2e}", (y_max + y_min) / 2.0),
+                    format!("{:.2e}", y_max),
+                ]),
         )
         .render(rect, buf);
 }
